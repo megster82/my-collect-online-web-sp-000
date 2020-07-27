58d9ca1f2f7e006d5c6b5560b116ca5be88d4ed5
meaghan jennings <meaghanljennings@gmail.com>
2020-07-27 15:02:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,12 @@
-def my_collect(students)
-  array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-  my_collect(array) do |name|
-    name.split(" ").first
-  end
+names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  
+def my_collect(array) 
+  i = 0
+  name_collection = []
+  while i < array.length
+  name_collection << yield(array[i])
+  i += 1
 end
+name_collection
+end
+my_collect(names) {|i| i.split(" ").first}
