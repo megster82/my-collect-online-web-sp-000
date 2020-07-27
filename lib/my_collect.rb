0cde3c056bbda29a1ee2842c850e4417073adcf8
meaghan jennings <meaghanljennings@gmail.com>
2020-07-27 15:06:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+names = ["Janet Evans", "Simone Biles", "Deena Kastor"]
+  
+def my_collect(array) 
+  i = 0
+  name_collection = []
+  while i < array.length
+  name_collection << yield(array[i])
+  i += 1
+end
+name_collection
+end
+my_collect(names) {|i| i.split(" ").first}
